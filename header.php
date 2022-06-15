63e8648e2ab264a177ad6f6f2c8ab9ac58c10e99
MuchaWatari <17173386+chum9625@users.noreply.github.com>
2022-06-15 22:28:47 +0900

    リンク修正

--- header.php
+++ header.php
@@ -32,7 +32,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
                   </label>
                   <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                     <li tabindex="0">
-                      <a href="https://demo.chum9625.com/nekozono/#update" class="justify-between">
+                      <a href="./" class="justify-between">
                         新着情報
                         <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                           <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
@@ -65,7 +65,7 @@ if ( !defined( 'ABSPATH' ) ) exit; ?>
               <div class="navbar-center hidden lg:flex">
                 <ul class="menu menu-horizontal p-0">
                   <li tabindex="0">
-                    <a href="https://demo.chum9625.com/nekozono/#update">
+                    <a href="./">
                       新着情報
                       <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                         <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
