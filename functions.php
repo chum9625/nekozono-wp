944b728040ab6f1b78c8708a2d226e85697ddd47
MuchaWatari <17173386+chum9625@users.noreply.github.com>
2022-06-17 10:40:00 +0900

    開発用にstyle.cssをget&update

--- functions.php
+++ functions.php
@@ -13,6 +13,7 @@ define("ZONO", get_template_directory_uri());
 */
 function script_init() {
   wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v6.1.1/css/all.css');
+  wp_enqueue_style('std_style', get_stylesheet_uri());//開発用
   wp_enqueue_style('my_style', 'https://demo.chum9625.com/dist/compiled.css');
   wp_enqueue_script('my_script', ZONO.'/js/main.js', array('jquery'));
 }
