e1cb9845f20397fd371b799b3ec5ddffe9fc5025
MuchaWatari <17173386+chum9625@users.noreply.github.com>
2022-07-02 12:13:26 +0900

    debug開始(VScode拡張機能整備)

--- index.php
+++ index.php
@@ -1,32 +1,40 @@
 <?php
 /**
  * Nekozono WordPress Theme
+ *
+ * @package WordPress
  * @author: ChumTech
  * @link: https://demo.chum9625.com/nekozono/
  */
-if ( !defined( 'ABSPATH' ) ) exit; ?>
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} ?>
 
 <?php get_header(); ?>
 
 <h1 class="py-6 text-gray-800 text-2xl sm:text-3xl font-bold text-center mb-4 md:mb-6">新着情報</h1>
 
 <section>
-  <?php get_template_part('template-parts/breadcrumb'); ?>
+	<?php get_template_part( 'template-parts/breadcrumb' ); ?>
 </section>
 
 <!-- news section start -->
 <div class="bg-white py-6 sm:py-8 lg:py-12">
-  <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
-    <div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 xl:gap-8">
-      <?php if( have_posts() ): ?>
-        <?php while( have_posts() ) : the_post(); ?>
+<div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+	<div class="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 md:gap-6 xl:gap-8">
+	<?php if ( have_posts() ) : ?>
+			<?php
+			while ( have_posts() ) :
+				the_post();
+				?>
 
-        <?php get_template_part('template-parts/loop'); ?>
+				<?php get_template_part( 'template-parts/loop' ); ?>
 
-        <?php endwhile; ?>
-        <?php endif; ?>
-    </div>
-  </div>
+		<?php endwhile; ?>
+		<?php endif; ?>
+	</div>
+</div>
 </div>
 
 <?php get_footer(); ?>
